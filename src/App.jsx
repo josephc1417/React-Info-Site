7bb91638e0abf5e1bbbdcb7fb5e146ad205ff23e
Joseph Clay <nubhunter57@Josephs-MacBook-Pro.local>
2023-08-25 04:14:00 -0500

    Took me forever to get this to render :)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.modules.css";
+import Navbar from "./Components/Navbar";
+import MC from "./Components/MC";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <MC />
+    </div>
+  );
+}
---
+++ src/Components/MC.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ss from "./MC.module.css";
+
+export default function MC() {
+  return <h1 className={ss.mc}>MainContent goes here</h1>;
+}
