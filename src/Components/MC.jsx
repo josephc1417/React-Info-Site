62c11e321a6f1ebd9c185106e05e8347ad729a34
Joseph Clay <nubhunter57@Josephs-MacBook-Pro.local>
2023-08-25 07:04:43 -0500

    Second Commit

--- src/Components/MC.jsx
+++ src/Components/MC.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import ss from "./MC.module.css";
 
 export default function MC() {
-  return <h1 className={ss.mc}>MainContent goes here</h1>;
+  return;
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-  return <h1 className={styles.nb}>Navbar goes here </h1>;
+  return (
+    <nav>
+      <img src="/src/assets/reactjs-icon.png" />
+      <h3>ReactFacts</h3>
+      <h4>React Course Project 1</h4>
+    </nav>
+  );
 }
