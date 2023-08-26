b85a55514c1e1b95894f2340ebf6e5c8b573954a
Joseph Clay <nubhunter57@Josephs-MacBook-Pro.local>
2023-08-26 00:17:45 -0500

    File Structure Completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,9 +4,9 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <nav>
-      <img class="nav--icon" src="/src/assets/reactjs-icon.png" />
-      <h3 class="nav--iconText">ReactFacts</h3>
-      <h4 class="nav--project">React Course-Project 1</h4>
+      <img className={styles["nav--icon"]} src="/src/assets/reactjs-icon.png" />
+      <h3 className={styles["nav--logo-text"]}>ReactFacts</h3>
+      <h4 className={styles["nav--project-text"]}>React Course-Project 1</h4>
     </nav>
   );
 }
