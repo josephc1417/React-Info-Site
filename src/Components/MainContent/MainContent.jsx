2de0f4fb36cd8f55c80a052d80923b35bafc7c86
Joseph Clay <nubhunter57@Josephs-MacBook-Pro.local>
2023-08-26 01:45:10 -0500

    Close

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -3,15 +3,15 @@ import styles from "./MainContent.module.css";
 
 export default function MainContent() {
   return (
-    <div className={styles.container}>
-      <h1>Fun Facts about React</h1>
-      <ul>
+    <main className={styles["main-container"]}>
+      <h1 className={styles["main--title"]}>Fun Facts about React</h1>
+      <ul className={styles["main--facts"]}>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100k stars on GitHub</li>
         <li>Is maintained by facebook </li>
         <li>Powers thousands of enterprise apps,including mobile apps</li>
       </ul>
-    </div>
+    </main>
   );
 }
